5ccc5c387b3326b848075d6d76af45ddf89e2550
Benjamin Wu <benjaminwu17@gmail.com>
2022-11-07 19:51:35 -0800

    Finished ws9

--- ws9/random.sh
+++ ws9/random.sh
@@ -2,12 +2,12 @@
 # Prints random lines from file
 AMOUNT=$1
 FILENAME=$2
-LINES=`wc -l $FILENAME | cut -d ` ` -f1`
+LINES=`wc -l $FILENAME | cut -d ' ' -f1`
 
-NUMBER=`expr $LINES \/ 100`
+NUMBER=`expr $LINES \/ 100 \* $AMOUNT`
 
 for ((i=0;i<$NUMBER;i++));
-do
-	OUTPUT=$(($RANDOM % $LINES))
-	echo `head -$OUTPUT $FILENAME | tail -1`
-done
+    do
+        OUTPUT=$(($RANDOM % $LINES))
+        echo `head -$OUTPUT $FILENAME | tail -1`
+    done
